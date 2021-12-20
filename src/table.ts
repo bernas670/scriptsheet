6e1b6e14f15929482986043ca42cc0319e001ea7
Bernardo Santos <bernardosantos670@gmail.com>
2021-12-20 14:20:50 +0000

    Fixed cell references and table display

--- src/table.ts
+++ src/table.ts
@@ -5,26 +5,27 @@ export default class Table {
     cells: Cell[][] = []
 
     constructor(public rows: number, public cols: number) {
-        for (let i = 0; i <= rows; i++) {
+        for (let row = 1; row <= rows; row++) {
             let tmpRow: Cell[] = []
-            for (let j = 0; j < cols; j++) {
-                tmpRow.push(new Cell(i + 1, String.fromCharCode(65+j)))
+            for (let i = 0; i < cols; i++) {
+                let col = String.fromCharCode(65 + i)
+                tmpRow.push(new Cell(row, col))
             }
             this.cells.push(tmpRow)
         }
     }
 
     display(): void {
-        let display: Array<Record<string, string | number>> = []
+        let display: Record<number, Record<string, string | number>> = {}
         for (let i = 0; i < this.cells.length; i++) {
             let tmp: Record<string, string | number> = {}
             for (let j = 0; j < this.cells[i].length; j++) {
                 tmp[String.fromCharCode(65 + j)] = this.cells[i][j].result
             }
-            display.push(tmp)
+            display[i+1] = tmp
         }
     
-        console.table(display)  
+        console.table(display)
     }
 
     getCell(row: number, col: string): Cell {
@@ -32,9 +33,9 @@ export default class Table {
     }
 
     checkCellRef(cell: string) {
-        let row = parseInt(cell[1], 10) - 1
+        let row = parseInt(cell[1], 10)
         let col = cell.toUpperCase().charCodeAt(0) - 65
 
-        return row >= 0 && row < this.rows && col >= 0 && col <= this.cols
+        return row > 0 && row <= this.rows && col >= 0 && col <= this.cols
     }
 }
