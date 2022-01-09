8546044a639929d1a32f6bb009330506f5b351ac
Bernardo Santos <bernardosantos670@gmail.com>
2022-01-09 01:39:59 +0000

    Started changing ui implementation to node repl

--- src/parser.ts
+++ src/parser.ts
@@ -1,28 +1,29 @@
 import * as P from 'parsimmon'
+import { alt, seq, string } from 'parsimmon'
 import * as F from "./formula"
 import Table from './table'
 import Cell from './cell'
 
-function range(start: number, end: number): number[] {
-    const result: number[] = []
-    let i = start
-    while (i <= end) {
-        result.push(i)
-        i += 1
-    }
-    return result
-}
 
 type Grammar = {
-    formula: F.Sum | F.Div | F.Mul | F.Sub | F.CellReference,
-    sumFormula: F.Sum,
-    rangeCells: Array<Cell>,
-    cellReference: F.CellReference,
-    cellFormula: Cell,
-    
-    // literals
-    stringLiteral: string,
-    numberLiteral: number,
+    // command: void,
+
+    // //commands
+    // commands: void,
+    help: void,
+    // display: void,
+
+    // // cell operations
+    // assign: void,
+    // formula: F.Sum | F.Div | F.Mul | F.Sub | F.CellReference,
+    // sumFormula: F.Sum,
+    // rangeCells: Array<Cell>,
+    // cellReference: F.CellReference,
+    // cellFormula: Cell,
+
+    // // literals
+    // letters: string,
+    // numbers: number,
 }
 
 export default class Parser {
@@ -31,30 +32,37 @@ export default class Parser {
 
     constructor(public table: Table) {
         this.language = P.createLanguage<Grammar>({
-            formula: l => P.alt(l.sumFormula, l.cellReference),
+            // command: l => alt(seq(string("/"), l.commands), l.assign),
 
-            cellReference: l => P.seq(l.cellFormula).map(([cell]) => new F.CellReference(cell)),
+            // // commands
+            // commands: l => alt(l.help, l.display),
+            help: _ => string("help").map(_ => console.log("help command output")),
+            // display: _ => string("display").map(_ => console.log("display command")),
 
-            cellFormula: l => P.seq(l.stringLiteral, l.numberLiteral)
-                .map(([col, row]) => this.table.getCell(row,col)),
+            // // table operations
+            // assign: l => P.seq(l.cellFormula, P.string("="), P.alt(l.formula, l.letters, l.numbers))
+            //     .map(([cell, _, value]) => cell.modifyCell(value)),
 
-            rangeCells: l => P.seq(l.cellFormula, P.string(':'), l.cellFormula)
-                .map(([a, _, b]) => this.table.getRange(a, b)),
+            // formula: l => P.alt(l.sumFormula, l.cellReference),
 
-            sumFormula: l => P.seq(P.string('SUM('), l.rangeCells, P.string(')'))
-                .map(([_, rangeArray, __]) => new F.Sum(...rangeArray)),
+            // cellFormula: l => P.seq(l.letters, l.numbers)
+            //     .map(([col, row]) => this.table.getCell(row, col)),
 
-            numberLiteral: _ => P.digits.map(row => parseInt(row, 10)),
-            stringLiteral: _ => P.letters
+            // cellReference: l => P.seq(l.cellFormula)
+            //     .map(([cell]) => new F.CellReference(cell)),
+
+            // rangeCells: l => P.seq(l.cellFormula, string(':'), l.cellFormula)
+            //     .map(([a, _, b]) => this.table.getRange(a, b)),
+
+            // sumFormula: l => P.seq(P.string('SUM('), l.rangeCells, P.string(')'))
+            //     .map(([_, rangeArray, __]) => new F.Sum(...rangeArray)),
+
+            // numbers: _ => P.digits.map(n => parseInt(n, 10)),
+            // letters: _ => P.letters
         })
     }
 
-    tryParse(str: string) {
-        return this.language.formula.tryParse(str)
+    tryParse(cmd: string) {
+        return this.language.help.tryParse(cmd)
     }
-
-    tryParseCell(str: string) {
-        return this.language.cellFormula.tryParse(str)
-    }
-
 }
--- src/scriptsheet.ts
+++ src/scriptsheet.ts
@@ -1,71 +1,53 @@
 import Parser from "./parser";
 import Table from "./table";
-import * as readline from 'readline'
-import { Formula } from "./formula";
-import Cell from "./cell";
+
+import repl from "repl"
+import { Context } from "vm"
 
 
 export default class ScriptSheet {
-    
+
     table: Table
     parser: Parser
 
     constructor() {
-        this.table = new Table(4,4)
+        this.table = new Table(4, 4)
         this.parser = new Parser(this.table)
     }
 
-    start() {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-            prompt: "> ",
-        })
-        
-        type InputState = 'cell' | 'formula'
-        let inputState: InputState = 'cell'
-        
-        console.clear()
-        
-        rl.setPrompt("cell > ")
+    eval(cmd: string, _context: Context, _filename: string, callback: (err: Error | null, result: any) => void) {
+        let result: any = "success"
+
+        try {
+            result = this.parser.tryParse(cmd)
+        } catch (error) {
+            result = error
+        }
+
         this.table.display()
-        rl.prompt()
 
-        let selectedCell: Cell
-        
-        rl.on('line', (line) => {
-            readline.moveCursor(process.stdout, 0,-1)
-            if (inputState === 'cell') {
-                if (this.table.checkCellRef(line)) {
-                    inputState = 'formula'
-                    rl.setPrompt(`f ${line.toUpperCase()} > `)
+        callback(null, result)
+    }
 
-                    selectedCell = this.parser.tryParseCell(line)
-                } else {
-                    console.error("Not a valid cell")
-                }
-            } else {
-                if (line.startsWith("=")) {
-                    try {
-                        let formula: Formula = this.parser.tryParse(line.slice(1))
-                        selectedCell.modifyCell(formula)
-                        inputState = 'cell'
-                        rl.setPrompt("cell > ")
-                        console.clear()
-                        this.table.display()
-                    } catch (error) {
-                        console.error(error)
-                    }
-                } else {
-                    inputState = 'cell'
-                    rl.setPrompt("cell > ")
-                    console.clear()
-                    selectedCell.modifyCell(line)
-                    this.table.display()
-                }
+    completer(line: string) {
+        var completions = 'help hello hi abc argh'.split(' ')
+        var hits = completions.filter(function (c) {
+            if (c.indexOf(line) == 0) {
+                return c;
             }
-            rl.prompt()
+        });
+        return [hits && hits.length ? hits : completions, line];
+    }
+
+    start() {
+        console.log("> Welcome to scriptsheet !")
+        console.log("> Here are some helpful commands to get you started :")
+
+        repl.start({
+            prompt: 'scriptsheet > ',
+            eval: this.eval.bind(this),
+            completer: this.completer.bind(this),
+            terminal: true
         })
     }
-    
 }
