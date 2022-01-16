1305d2d7a94cba45b1055d3c94aa590f1e7b40ef
Bernardo Santos <bernardosantos670@gmail.com>
2022-01-16 18:49:53 +0000

    Refactored if formula

--- src/formula.ts
+++ src/formula.ts
@@ -8,7 +8,6 @@ export abstract class Formula {
         this.dependsOn = new Set<Cell>(dependsOn)
     }
 
-    // FIXME: what can we do about this
     execute(): number | string {
         return ""
     }
@@ -128,7 +127,7 @@ export class Avrg extends Formula {
 
 export class Arithmetic extends Formula {
 
-    constructor(public arg1: Cell | number, public op: String, public arg2: Cell | number) {
+    constructor(public arg1: Cell | number, public op: string, public arg2: Cell | number) {
         let as: Array<Cell> = []
         if (arg1 instanceof Cell) as.push(arg1)
         if (arg2 instanceof Cell) as.push(arg2)
@@ -159,124 +158,42 @@ export class Arithmetic extends Formula {
     }
 }
 
+type IfArg = string | number | Cell | Formula
+
 export class If extends Formula {
 
-    constructor(public arg1: Cell | number | Formula, 
-                public op: string, 
-                public arg2: Cell | number | Formula, 
-                public result1: string | number | Cell | Formula, 
-                public result2: string | number | Cell | Formula) {
+    args: IfArg[]
+    outs: IfArg[]
 
-        let as: Array<Cell> = []
-        if (arg1 instanceof Cell) as.push(arg1)
-        if (arg2 instanceof Cell) as.push(arg2)
-        if (result1 instanceof Cell) as.push(result1)
-        if (result2 instanceof Cell) as.push(result2)
-        if (arg1 instanceof Formula) {
-            let aux: Formula = arg1 as Formula
+    constructor(arg1: IfArg, public op: string, arg2: IfArg, out1: IfArg, out2: IfArg) {
+        let as: Cell[] = []
+        for (let i = 0; i < arguments.length; i++) {
+            const arg = arguments[i]
 
-                for(let cell of aux.dependsOn) {
-                    as.push(cell as Cell)
-                }
+            if (arg instanceof Cell)
+                as.push(arg)
+            else if (arg instanceof Formula)
+                as.push(...arg.dependsOn)
         }
-        if (arg2 instanceof Formula) {
-            let aux: Formula = arg2 as Formula
-
-                for(let cell of aux.dependsOn) {
-                    as.push(cell as Cell)
-                }
-        }
-        if (result1 instanceof Formula) {
-            let aux: Formula = result1 as Formula
-
-                for(let cell of aux.dependsOn) {
-                    as.push(cell as Cell)
-                }
-        }
-        if (result2 instanceof Formula) {
-            let aux: Formula = result2 as Formula
-
-                for(let cell of aux.dependsOn) {
-                    as.push(cell as Cell)
-                }
-        }
-
         super(as)
+
+        this.args = [arg1, arg2]
+        this.outs = [out1, out2]
     }
 
     execute(): string | number {
-        
-        let value1: number
-        let value2: number
-        let output1: string | number
-        let output2: string | number
+        const vals: (string | number)[] = this.args.map(arg => this.getArgValue(arg))
+        const result = eval(`${vals[0]} ${this.op} ${vals[1]}`)
+        const output = result ? this.outs[0] : this.outs[1]
 
-        if (this.arg1 instanceof Cell) {
-            if (isNaN(+this.arg1.result)) {
-                throw new SCError('#INVALID!')
-            }
-            value1 = this.arg1.result as number
-        } else if(this.arg1 instanceof Formula) {
-            value1 = this.arg1.execute() as number
-        } else value1 = this.arg1
-
-        if (this.arg2 instanceof Cell) {
-            if (isNaN(+this.arg2.result)) {
-                throw new SCError('#INVALID!')
-            }
-            value2 = this.arg2.result as number
-        } else if(this.arg2 instanceof Formula) {
-            value2 = this.arg2.execute() as number
-        } else value2 = this.arg2
-
-        if (this.result1 instanceof Cell) {
-            // if (isNaN(+this.result1.result)) {
-            //     throw new SCError('#INVALID!')
-            // }
-            output1 = this.result1.result 
-        } else if(this.result1 instanceof Formula) {
-            output1 = this.result1.execute() as number
-        } else output1 = this.result1
-
-        if (this.result2 instanceof Cell) {
-            // if (isNaN(+this.result2.result)) {
-            //     throw new SCError('#INVALID!')
-            // }
-            output2 = this.result2.result
-        } else if(this.result2 instanceof Formula) {
-            output2 = this.result2.execute() as number
-        } else output2 = this.result2
-
-
-        switch(this.op){
-            case ">": {
-                if(value1 > value2) return output1
-                else return output2
-            }
-            case "<": {
-                if(value1 < value2) return output1
-                else return output2
-            }
-            case "==": {
-                if(value1 == value2) return output1
-                else return output2
-            }
-            case "!=": {
-                if(value1 != value2) return output1
-                else return output2
-            }
-            case ">=": {
-                if(value1 >= value2) return output1
-                else return output2
-            }
-            case "<=": {
-                if(value1 <= value2) return output1
-                else return output2
-            }
-            default: {
-                throw new SCError('#INVALID!')
-            }
-        }
+        return this.getArgValue(output)
     }
 
+    getArgValue(arg: IfArg): string | number {
+        if (arg instanceof Cell)
+            return arg.result
+        else if (arg instanceof Formula)
+            return arg.execute()
+        return arg
+    }
 }
